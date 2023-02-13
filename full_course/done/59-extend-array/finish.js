1c529f6bbf309fbcaa780e7985fde8e56586b93a
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-02-13 08:09:36 +0300

    js tasks

---
+++ full_course/done/59-extend-array/finish.js
@@ -0,0 +1,35 @@
+/** ЗАДАЧА 59 - Расширение массивов
+ *
+ * 1. Создайте новый класс "ExtendedArray", который должен расширять встроенный "Array"
+ *
+ * 2. Добавьте в новый класс два пользовательских метода:
+ *  - "sum" - он должен возвращать сумму всех элементов массива
+ *  - "onlyNumbers" - должен возвращать новый массив,
+ * который будет содержать только числа из исходного массива
+ *
+ * 3. Создайте несколько экземпляров нового класса "ExtendedArray"
+ * и протестируйте оба метода "sum" и "onlyNumbers".
+ *
+ * 4. Убедитесь, что остальные методы массивов такие,
+ * как "forEach", "map" также доступны
+ */
+
+class ExtendedArray extends Array{
+    sum() {
+        return this.reduce((a, x) => a + x, 0);
+    }
+    onlyNumbers() {
+        return this.filter(el => typeof el === 'number');
+    }
+}
+
+let myNew = new ExtendedArray(1, 2, 3, 4, 5)
+console.log(myNew)
+console.log(myNew.sum())
+myNew.push('a', 'g', 'q');
+console.log(myNew)
+console.log(myNew.onlyNumbers())
+
+console.log(myNew.sum())
+myNew.forEach(el => console.log(el + 1))
+console.log(myNew)
