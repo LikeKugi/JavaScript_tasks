a63d682042191f1d0db92ee91e20a9e3eab614a0
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-04 17:03:41 +0300

    collision added

--- JavaScript_Games/p5/js/main.js
+++ JavaScript_Games/p5/js/main.js
@@ -1,7 +1,8 @@
 // ---------------------------------------------------------------------------------------
 // variables and constants
 // ---------------------------------------------------------------------------------------
-
+// game difficult
+var difficult = new Difficult();
 // bird image
 var birdImg;
 // canvas size
@@ -10,8 +11,15 @@ var canvasY = 768;
 
 var bird = new Bird();
 let barriers = [];
+var progressingGame = true;
+
 
 const randomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const resetGame = () => {
+    resetObstructions();
+    bird.init();
+    progressingGame = true;
+}
 
 // ---------------------------------------------------------------------------------------
 // SETUP and DRAWING
@@ -23,13 +31,13 @@ function setup() {
 }
 
 function draw() {
+    if (!progressingGame) return;
     background(230, 230, 200);
     image(birdImg, bird.x, bird.y, 50, 50);
     bird.changingPosition();
 
     rectMode(CORNERS);
     obstructions();
-
 }
 
 // ---------------------------------------------------------------------------------------
@@ -38,10 +46,10 @@ function draw() {
 
 function Bird() {
     // bird position
-    this.x = 150;
-    this.y = 50;
+    this.x = canvasX / 4;
+    this.y = canvasY / 4;
     this.yV = 1;
-    this.accel = 0.05;
+    this.accel = 0.07 + difficult.level/50;
 }
 Bird.prototype.changingAcceleration = function (down = true) {
     if (down) {
@@ -49,7 +57,8 @@ Bird.prototype.changingAcceleration = function (down = true) {
         if (canvasY - this.y < 50) this.yV = 0;
     } else {
         this.yV = 1;
-        this.yV -= 90 * this.accel;
+        this.yV -= (90 * bird.accel) < 5 ? (90 * bird.accel) : 4.95;
+        console.log(this.yV, bird.accel)
     }
 }
 Bird.prototype.changingPosition = function () {
@@ -58,7 +67,18 @@ Bird.prototype.changingPosition = function () {
 
     this.changingAcceleration()
 }
-
+Bird.prototype.init = function () {
+    this.x = canvasX / 4;
+    this.y = canvasY / 4;
+    this.yV = 1;
+    this.initAccel();
+}
+Bird.prototype.initAccel = function () {
+    this.accel = 0.05 + difficult.level/400;
+}
+Bird.prototype.getCoords = function () {
+    return [this.x - 10, this.y - 10, this.x + 10, this.y + 10];
+}
 // ---------------------------------------------------------------------------------------
 // Barrier and its logic
 // ---------------------------------------------------------------------------------------
@@ -66,16 +86,20 @@ Bird.prototype.changingPosition = function () {
 function Rectangles() {
     this.x = canvasX;
     this.y1 = randomInteger(canvasY / 5, canvasY / 3);
+    this.y2 = this.y1 + randomInteger(200, 300);
     this.color = [randomInteger(10, 250), randomInteger(10, 250), randomInteger(10, 250)]
 }
 Rectangles.prototype.changingPosition = function () {
-    this.x -= 1;
+    this.x -= (1 + 0.5 * difficult.level);
 }
 Rectangles.prototype.UP = function () {
-    return [this.x, 0, this.x+10, this.y1];
+    return [this.x, 0, this.x + 10, this.y1];
 }
 Rectangles.prototype.DOWN = function () {
-    return [this.x, this.y1 + 200, this.x+10, canvasY];
+    return [this.x, this.y2, this.x + 10, canvasY];
+}
+Rectangles.prototype.getHole = function () {
+    return [this.x, this.y1, this.x+10, this.y2]
 }
 // ---------------------------------------------------------------------------------------
 // obstructions and its logic
@@ -86,10 +110,18 @@ function getFence() {
 }
 
 function obstructions() {
+    let birdCoords = bird.getCoords();  // [bx1, by1, bx2, by2]
     if (barriers.length === 0) {
         barriers.push(getFence());
     } else {
         barriers.forEach((el, i)=> {
+            let elCoords = el.getHole();
+            if (Math.abs(elCoords[0] - birdCoords[0]) < 10 || Math.abs(elCoords[2] - birdCoords[2]) < 10) {
+                if (!checkCollision(birdCoords, elCoords)) {
+                    console.log('collision');
+                }
+            }
+
             fill(...el.color);
             rect(...el.UP());
             rect(...el.DOWN());
@@ -98,23 +130,50 @@ function obstructions() {
                 barriers.splice(i, 1);
             }
         })
-        if (canvasX - barriers[barriers.length - 1].x > 150) {
+        if (canvasX - barriers[barriers.length - 1].x > (150 + 10 * difficult.level)) {
             barriers.push(getFence());
         }
     }
-
+}
+function checkCollision(birdCoords, barrierCoords) {
+    return birdCoords[1] > barrierCoords[1] && birdCoords[3] < barrierCoords[3]
+}
+function resetObstructions() {
+    barriers.length = 0;
+}
+// ---------------------------------------------------------------------------------------
+// DIFFICULT level logic
+// ---------------------------------------------------------------------------------------
+function Difficult() {
+    this.level = 3;
+}
+Difficult.prototype.increase = function () {
+    this.level += 1;
+}
+Difficult.prototype.decrease = function () {
+    this.level > 1 ? this.level -= 1 : this.level = 0;
+}
+function increaseLevel() {
+    this.difficult.increase();
+    bird.initAccel();
+}
+function decreaseLevel() {
+    this.difficult.decrease();
+    bird.initAccel();
 }
 // ---------------------------------------------------------------------------------------
 // keypressed and its logic
 // ---------------------------------------------------------------------------------------
 function keyPressed() {
-    console.log(bird.x, bird.y)
     if (keyCode === LEFT_ARROW) {
-        bird.x -= 10;
+        decreaseLevel();
     } else if (keyCode === RIGHT_ARROW) {
-        bird.x += 10;
+        increaseLevel();
     } else if (keyCode === UP_ARROW) {
-        bird.y -= 90;
+        progressingGame = false;
+        setTimeout(resetGame, 3000);
+    } else if (keyCode === DOWN_ARROW) {
+        progressingGame = !progressingGame;
     } else if (keyCode === 32) {
         bird.changingAcceleration(false);
     }
