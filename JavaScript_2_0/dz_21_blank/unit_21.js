6afabefe66e3309c65442b2b222da5014aed5414
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-06 23:18:31 +0300

    unit 21 touch events

--- JavaScript_2_0/dz_21_blank/unit_21.js
+++ JavaScript_2_0/dz_21_blank/unit_21.js
@@ -10,73 +10,111 @@ document.querySelector(".div-1").addEventListener("touchstart", t1);
 // Task 2 ============================================
 /* Создайте блок div-2. Добавьте на него событие touchstart. Выведите в out-2 число срабатываний события. */
 
-function t2() {}
+function t2(e) {
+  console.log("here");
+  console.log(e.touches.length);
+  document.querySelector(".out-2").textContent = e.changedTouches.length;
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-2").addEventListener("touchstart", t2);
 // Task 3 ============================================
 /*  Создайте блок div-3_1 и div-3_2. Добавьте на них событие touchstart. Выведите в out-3 номер блока 1 или 2 на котором сработало событие. */
 
-function t3() {}
+function t3() {
+  document.querySelector(".out-3").textContent = this.textContent;
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-3_1").addEventListener("touchstart", t3);
+document.querySelector(".div-3_2").addEventListener("touchstart", t3);
 // Task 4 ============================================
 /*  Создайте блок div-4. И кнопку b-4. При нажатии кнопки - добавляйте событие ontouchstart на блок div-4. При событии происходит вывод текста touch в out-4.  */
+function toggleTouchEvent() {
+  let eventElem = document.querySelector(".div-4");
+  eventElem.addEventListener("touchstart", t4);
+}
 
-function t4() {}
+function t4() {
+  document.querySelector(".out-4").textContent = this.textContent;
+}
 
 // ваше событие здесь!!!
+document.querySelector(".b-4").addEventListener("click", toggleTouchEvent);
 
 // Task 5 ============================================
 /*  Дана кнопка b-5. При ее нажатии очищайте событие ontouchstart на блоке div-4. */
 
-function t5() {}
+function t5() {
+  document.querySelector(".div-4").removeEventListener("touchstart", t4);
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".b-5").addEventListener("click", t5);
 // Task 6 ============================================
 /*  Добавьте событие ontouchend на div-4. При его срабатывании выведите в out-6 слово touchend. */
 
-function t6() {}
+function t6() {
+  document.querySelector(".out-6").textContent = "touch end";
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-4").addEventListener("touchend", t6);
 // Task 7 ============================================
 /*  Дан блок div-7. Добавьте событие touch, при срабатывании которого окрашивайте блок в красный цвет. */
 
-function t7() {}
+function t7() {
+  this.style.backgroundColor = "red";
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-7").addEventListener("touchstart", t7);
 // Task 8 ============================================
 /*  Дан блок div-8. Добавьте на него событие touch, которое при срабатывании окрашивает блок случаным цветом из массива a8=[red, green, blue, orange, pink, yellow] */
 
-function t8() {}
+function t8() {
+  let a8 = ["red", "green", "blue", "orange", "pink", "yellow"];
+  const randomInt = (min = 0, max = a8.length - 1) =>
+    Math.floor(Math.random() * (max - min + 1));
+  this.style.backgroundColor = a8[randomInt()];
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-8").addEventListener("touchstart", t8);
 // Task 9 ============================================
 /* Дан блок div-9. Добавьте событие ontouch. Выводите количество одновременных касаний в out-9. */
 
-function t9() {}
+function t9(e) {
+  document.querySelector(".out-9").textContent = e.touches.length;
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-9").addEventListener("touchstart", t9);
 // Task 10 ============================================
 /*  Дан блок div-10. Добавьте на него событие touchmove. При срабатывании события - увеличивайте его ширину на 1. */
 
-function t10() {}
+function t10() {
+  console.log("here");
+  let width =
+    parseInt(this.style.width) || parseInt(getComputedStyle(this).width);
+  this.style.width = width + 1 + "px";
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-10").addEventListener("touchmove", t10);
 // Task 11 ============================================
 /*  Дан блок div-11. Добавьте на него событие touch. При срабатывании выводите радиус события radiusX, radiusY. */
 
-function t11() {}
+function t11(e) {
+  console.log(e.touches[0].radiusX);
+  document.querySelector(
+    ".out-11"
+  ).innerHTML = `radiusX: ${e.touches[0].radiusX}<br>
+  radiusY: ${e.touches[0].radiusY}`;
+}
 
 // ваше событие здесь!!!
-
+document.querySelector(".div-11").addEventListener("touchstart", t11);
 // Task 12 ============================================
 /*  Мини проект. Ознакомьтесь с версткой в задании 12.
 <p>Изучите html код внутри div-12-wrapper.</p>
@@ -90,14 +128,35 @@ function t11() {}
 const images = document.querySelectorAll(".img-12-min");
 let count = 0; // переменная, которая указывает на номер активного изображения в images
 
-const next = document.querySelectorAll(".next");
-next.onclick = nextFunction;
+const next = document.querySelector(".next");
+next.addEventListener("click", nextFunction);
+next.addEventListener("touchstart", nextFunction);
 
-const prev = document.querySelectorAll(".prev");
-prev.onclick = prevFunction;
+const prev = document.querySelector(".prev");
+prev.addEventListener("click", prevFunction);
+prev.addEventListener("touchstart", prevFunction);
 
-function nextFunction() {}
+function nextFunction() {
+  count = (count + 1) % images.length;
+  setActive();
+}
 
-function prevFunction() {}
+function prevFunction() {
+  count = count - 1 >= 0 ? (count - 1) % images.length : images.length - 1;
+  setActive();
+}
 
 // ваше событие здесь!!!
+function setActive() {
+  images.forEach((el, i) => {
+    if (i === count) {
+      el.classList.add("active-img");
+      setMaxActive(el.src);
+    } else {
+      el.classList.remove("active-img");
+    }
+  });
+}
+function setMaxActive(src) {
+  document.querySelector(".img-12-max").src = src;
+}
