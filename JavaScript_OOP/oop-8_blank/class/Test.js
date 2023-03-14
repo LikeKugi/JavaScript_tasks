ca559f50193287563fca415941451991bfae22fe
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-03-15 02:53:53 +0300

    changed gitignore and test for timestamp

--- JavaScript_OOP/oop-8_blank/class/Test.js
+++ JavaScript_OOP/oop-8_blank/class/Test.js
@@ -1,21 +1,21 @@
 class Test {
-    static b = "ItGid.info";
+  static b = "ItGid.info";
 
-    constructor(a) {
-        this.a = a;
-    }
+  constructor(a) {
+    this.a = a;
+  }
 
-    someMethod() {
-        console.log('classic function');
-        return this.a;
-    }
+  someMethod() {
+    console.log("classic function");
+    return this.a;
+  }
 
-    someMethod2 = () => {
-        console.log('arrow function');
-        return this.a;
-    }
+  someMethod2 = () => {
+    console.log("arrow function");
+    return this.a;
+  };
 
-    static staticMethod() {
-        return this.b;
-    }
+  static staticMethod() {
+    return this.b;
+  }
 }
