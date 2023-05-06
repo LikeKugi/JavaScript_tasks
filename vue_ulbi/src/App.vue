391afc76a6d3a09f443c42fc2ea5bf85cb01d5d1
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-05-06 14:29:42 +0300

    lesson 1

---
+++ vue_ulbi/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+ <div class="content">
+   <h1>hello</h1>
+   <div class="counter">Count likes: {{ likes }}</div>
+   <button @click="addLike">like</button>
+   <div class="counter">Count dislikes: {{ dislikes }}</div>
+   <button @click="addDislike">dislike</button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
